34780efc5db7588df1a535b71b216aabe84e11bd
codingcele <marcocelegato9@gmail.com>
2023-04-14 14:43:53 +0200

    Aggiungere i file di progetto.

---
+++ CalcoliHelper.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcolatrice
+{
+
+    public class CalcoliHelper
+    {
+        public static int Sum(int num1, int num2)
+        {
+            Console.WriteLine(num1 + num2);
+            return num1 + num2;
+        }
+        public static double Sum(double num1, double num2)
+        {
+            Console.WriteLine(num1 + num2);
+            return num1 + num2;
+        }
+        public static int Subtract(int num1, int num2)
+        {
+            Console.WriteLine(num1 - num2);
+            return num1 - num2;
+        }
+        public static double Subtract(double num1, double num2)
+        {
+            Console.WriteLine(num1 - num2);
+            return num1 - num2;
+        }
+        public static int Multiply(int num1, int num2)
+        {
+            Console.WriteLine(num1 * num2);
+            return num1 * num2;
+        }
+        public static double Multiply(double num1, double num2)
+        {
+            Console.WriteLine(num1 * num2);
+            return num1 * num2;
+        }
+        public static int Abs(int num)
+        {
+            if (num < 0)
+            {
+                Console.WriteLine(-num);
+                return -num;
+            }
+            else
+            {
+                Console.WriteLine(num);
+                return num;
+            }
+        }
+        public static double Abs(double num)
+        {
+            if (num < 0)
+            {
+                Console.WriteLine(-num);
+                return -num;
+            }
+            else
+            {
+                Console.WriteLine(num);
+                return num;
+            }
+        }
+        public static int Min(int num1, int num2)
+        {
+            if (num1 == num2)
+            {
+                Console.WriteLine(777777777);
+                return 777777777;
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine(num1);
+                return num1;
+            }
+            else
+            {
+                Console.WriteLine(num2);
+                return num2;
+            }
+        }
+        public static double Min(double num1, double num2)
+        {
+            if (num1 == num2)
+            {
+                Console.WriteLine(777777777);
+                return 777777777;
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine(num1);
+                return num1;
+            }
+            else
+            {
+                Console.WriteLine(num2);
+                return num2;
+            }
+        }
+        public static int Max(int num1, int num2)
+        {
+            if (num1 == num2)
+            {
+                Console.WriteLine(777777777);
+                return 777777777;
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine(num2);
+                return num2;
+            }
+            else
+            {
+                Console.WriteLine(num1);
+                return num1;
+            }
+        }
+        public static double Max(double num1, double num2)
+        {
+            if (num1 == num2)
+            {
+                Console.WriteLine(777777777);
+                return 777777777;
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine(num2);
+                return num2;
+            }
+            else
+            {
+                Console.WriteLine(num1);
+                return num1;
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcolatrice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CalcoliHelper.Sum(1,3);
+            CalcoliHelper.Sum(1.15, 3.6);
+
+            CalcoliHelper.Subtract(1, 3);
+            CalcoliHelper.Subtract(1.15, 3.6);
+
+            CalcoliHelper.Multiply(1, 3);
+            CalcoliHelper.Multiply(1.15, 3.6);
+
+            CalcoliHelper.Abs(3);
+            CalcoliHelper.Abs(-2);
+            CalcoliHelper.Abs(1.15);
+            CalcoliHelper.Abs(-3.15);
+
+            CalcoliHelper.Min(1, 3);
+            CalcoliHelper.Min(5, 2);
+            CalcoliHelper.Min(3.6, 1.15);
+            CalcoliHelper.Min(2.8, 6.7);
+
+            CalcoliHelper.Max(1, 3);
+            CalcoliHelper.Max(5, 2);
+            CalcoliHelper.Max(3.6, 1.15);
+            CalcoliHelper.Max(2.8, 6.7);
+        }
+    }
+}
